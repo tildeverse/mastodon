8d91fc267d0c9a2d20168cc215197eb27e7bfa8e
Ben Harris <ben@tilde.team>
2018-05-01 12:59:30 -0400

    sourceurl

---
+++ config/initializers/source.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Mastodon
+  module Version
+    module_function
+    def source_base_url
+      'https://github.com/tildeverse/mastodon'
+    end
+  end
+end
+
